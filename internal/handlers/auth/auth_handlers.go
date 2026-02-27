fc1d970b4f97ff53bb8eb88bce1e79f422c5bd7b
Alexander Pooler <alexpooler03@gmail.com>
2026-02-27 22:42:36 +0000

    Set nil/null roles to default role

--- internal/handlers/auth/auth_handlers.go
+++ internal/handlers/auth/auth_handlers.go
@@ -117,6 +117,9 @@ func (p *AuthProvider) Me(w http.ResponseWriter, r *http.Request) {
 		user["id"] = oid
 	}
 	if roles, ok := claims["roles"]; ok {
+		if roles == nil || len(roles.([]string)) == 0 {
+			roles = []string{"default"}
+		}
 		user["roles"] = roles
 	}
 
